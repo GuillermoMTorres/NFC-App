317515c21d55eb642e547b87cdda0dc15a16f43b
GuillermoMTorres <Shadowete2@gmail.com>
2017-09-24 22:00:47 +0200

    Upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { NFC, Ndef } from '@ionic-native/nfc';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCcN5bOptWxK0QSKQFMQEJGC14DJ-MOs5s",
+    authDomain: "nfcwebsite-ad1be.firebaseapp.com",
+    databaseURL: "https://nfcwebsite-ad1be.firebaseio.com",
+    projectId: "nfcwebsite-ad1be",
+    storageBucket: "nfcwebsite-ad1be.appspot.com",
+    messagingSenderId: "721440709193"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +28,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig,'demo104'),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +41,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    NFC,
+    Ndef,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,66 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, AlertController } from 'ionic-angular';
+import { NFC, Ndef } from '@ionic-native/nfc';
+import { FirebaseListObservable, AngularFireDatabase  } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage {	
 
-  constructor(public navCtrl: NavController) {
+  tasks: FirebaseListObservable<any>;
 
+  todo = {
+  	DNI: "",
+  	Cantidad: "",
   }
+
+  text = ""
+
+  text2 = "No hay NFC";
+  	logForm(){
+    	this.text = "Current DNI: " + this.todo.DNI;
+      this.text2 = "Current Cant: " + this.todo.Cantidad;
+    }	  
+
+  constructor(
+	  private alertCtrl: AlertController,
+	  public navCtrl: NavController,
+    public fireDatabase: AngularFireDatabase,
+	  nfc: NFC,
+	  ndef: Ndef,)
+	{  	
+      this.tasks = this.fireDatabase.list('/NFC');
+
+      nfc.addNdefListener(() => {
+
+      }, (err) => {
+        this.text2 = "error"
+      }).subscribe((data) => {
+        var message = [
+            ndef.textRecord(this.todo.DNI),
+            ndef.uriRecord("http://gmedinatorres.es")
+        ];
+      nfc.write(message);
+        this.tasks.push({
+              DNI: this.todo.DNI,
+              Cantidad: this.todo.Cantidad
+         });
+      });
+ 	}
+
+
+  updateTask( task ){
+    this.tasks.update( task.$key,{
+      title: task.title,
+      done: !task.done
+    });
+  }
+
+  removeTask( task ){
+    this.tasks.remove( task.$key );
+  }
+  	
 
 }
